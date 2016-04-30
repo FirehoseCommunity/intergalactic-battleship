a8754ab49b690e15f0531dfb9959579b277e9623
katschre <schreiber.katrina@gmail.com>
2016-04-30 00:42:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntergalacticBattleship::Application.config.secret_key_base = 'c48024577ceaecbe3766c6f07ab8a08b902df5dc970630b9930c05a1b45d23a12c1ac1380fc6fcc0d0efcf888716304092b157b8c6bebdb448c1ac35b2e119a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IntergalacticBattleship::Application.config.session_store :cookie_store, key: '_intergalactic-battleship_session'
