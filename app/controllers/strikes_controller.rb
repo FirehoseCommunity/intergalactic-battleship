45e4fa10d6804ff79b913c1826c8d05a41d8d0c0
Kamakshi <kpat108@gmail.com>
2016-05-07 03:02:21 +0000

    added model for strike

---
+++ app/controllers/strikes_controller.rb
@@ -0,0 +1,18 @@
+class StrikesController < ApplicationController
+  
+  def new
+    @strike = Strike.new
+  end
+
+  def index
+  end
+
+  def create
+    @strike = Strike.create(strike_params)
+    redirect_to root_path
+  end
+
+  def strike_params
+    params.require(:strike).permit(:hit, :x_coordinate, :y_coordinate)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160507014601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "strikes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "x_coordinate"
+    t.integer  "y_coordinate"
+    t.integer  "game_id"
+    t.boolean  "hit"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
