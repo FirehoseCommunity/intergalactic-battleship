2949f54b53706eb56a00b27be5605ffa1e7eefb7
katschre <schreiber.katrina@gmail.com>
2016-05-11 23:13:44 +0000

    adding game model w foreign keys

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,8 @@
 class Game < ActiveRecord::Base
 
+  belongs_to :jedi_user_id, foreign_key: 'sith_user_id', class_name: "User"
+  belongs_to :sith_user_id, foreign_key: 'jedi_user_id', class_name: "User"
+  has_many :ships
 
-  
 end
+
