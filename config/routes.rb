4d7804668087977b13f8cbc36efa3c37d19ec461
katschre <schreiber.katrina@gmail.com>
2016-05-12 04:27:12 +0000

    commenting out root files so code works, not sure which to keep

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 IntergalacticBattleship::Application.routes.draw do
 
   root 'games#index'
-  root "strikes#index"
+  #root "strikes#index"
   resources :strikes, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  #root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20160507014601) do
     t.integer  "y_coordinate"
     t.integer  "game_id"
     t.boolean  "hit"
-
     t.datetime "created_at"
     t.datetime "updated_at"
   end
