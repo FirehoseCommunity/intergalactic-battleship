c085fa76f6a59131eb89b65b7aeea4cb4621daa6
katschre <schreiber.katrina@gmail.com>
2016-05-12 04:04:39 +0000

    adjusting foreign key per comments

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
 
-  belongs_to :jedi_user_id, foreign_key: 'sith_user_id', class_name: "User"
-  belongs_to :sith_user_id, foreign_key: 'jedi_user_id', class_name: "User"
+  belongs_to :jedi_user_id, foreign_key: 'jedi_user_id', class_name: "User"
+  belongs_to :sith_user_id, foreign_key: 'sith_user_id', class_name: "User"
   has_many :ships
 
 end
