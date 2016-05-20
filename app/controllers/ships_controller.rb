26decf6b222aa73c63d421da63f9943343815c7d
Robert Sapunarich <robertsapunarich@gmail.com>
2016-05-20 16:02:28 -0700

    Add `ships_controller#create` action. TODO: Need to resolve undefined method error for `permit`.

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -2,5 +2,15 @@ class ShipsController < ApplicationController
 
   def index
   end
+
+  def create
+    @ship = Ship.create(ship_params)
+  end
+
+  private
+
+  def ship_params
+    params.require(:ship).permit(:game_id, :user_id)
+  end
   
 end
--- spec/controllers/ships_controller_spec.rb
+++ spec/controllers/ships_controller_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe ShipsController, type: :controller do
     it "should successfully create a ship" do
       post :create, ship: FactoryGirl.create(:ship)
       expect(response).to have_http_status(:success)
+      star_destroyer = Ship.last
+      expect(star_destroyer.vertical?).to eq(true)
+      expect(star_destroyer.x_coordinate).to eq(3)
+      expect(star_destroyer.y_coordinate).to eq(4)
     end
   end
 
