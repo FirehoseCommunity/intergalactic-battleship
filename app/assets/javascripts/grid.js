618de360100a8d144c18d8288f310e12e1179b2b
Kamakshi <kpat108@gmail.com>
2016-10-05 01:28:36 +0000

    --skip-ci create battleship board

---
+++ app/assets/javascripts/grid.js
@@ -0,0 +1,34 @@
+$(function() {
+
+ 
+  
+  $('.moveable').draggable({
+    stack: '.battleship-grid',
+    cursor:'crosshair',
+    revert: 'invalid'
+    
+  });
+
+  
+  $('td').droppable({
+    drop: handleDrop 
+
+  });
+
+  function handleDrop(event, ui) {
+    var droppedShip = ui.draggable;
+    droppedShip.position({
+      of: $(this),
+      my: 'left top',
+      at: 'left top'
+    });
+    droppedShip.draggable('option', 'revert', false);
+    droppedShip.draggable('option', 'grid', [46, 46]);
+
+    $('.moveable').click(function() {
+    $(this).toggleClass('rotate');
+    });
+  }
+
+
+});
