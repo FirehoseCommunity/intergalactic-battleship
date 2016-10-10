6651daea86d961e780ba2e3754316ecdd6029ac7
Amanda Mark <amarkpark@gmail.com>
2016-10-10 02:10:43 +0000

    Fixed games#update & view so can't join own game as player2. Revised & added tests. ~AMP

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,11 +22,12 @@ class GamesController < ApplicationController
   end
 
   def update
+    #this needs to be cleaned up seriously using private game_params
     @game = Game.find(params[:id])
-    if current_user.id == params[:jedi_user_id].to_i
+    if current_user.id != params[:jedi_user_id].to_i
       @game.update(sith_user_id: current_user.id)
       redirect_to ships_path
-    elsif current_user.id == params[:sith_user_id].to_i
+    elsif current_user.id != params[:sith_user_id].to_i
       @game.update(jedi_user_id: current_user.id)
       redirect_to ships_path
     else
@@ -35,6 +36,8 @@ class GamesController < ApplicationController
   end
 
   private
+
+
 
   # helper_method :find_empty_user
 
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,113 +2,101 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
-  #insert before actions here to create user
-  #let command
+  let(:user) { FactoryGirl.create(:user) }
+  let(:user2) { FactoryGirl.create(:user) }
+  # before(:each) do
+  #   user = FactoryGirl.create(:user)
+  #   user2 = FactoryGirl.create(:user)
+  # end
+
   describe "games#index action" do
     
-    it "should successfully show the page" do
-      user = FactoryGirl.create(:user)
+    it "will successfully show the page" do
       sign_in user
-
       get :index
+
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "games#new action" do
-    it "should require user to be logged in" do
+    it "will require user to be logged in" do
       get :new
+
       expect(response).to redirect_to new_user_session_path
     end
 
-    it "should successfully show the pick a side page" do
-      user = FactoryGirl.create(:user)
+    it "will successfully show the pick a side page" do
       sign_in user
-
       get :new
+
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "games#create action" do
-    it "should require users to be logged in" do
-      
+    it "will require users to be logged in" do
       post :create, sith_user_id: 1
+
       expect(response).to redirect_to new_user_session_path
     end
  
-    it "should successfully create a new game in our database as Sith" do
-      user = FactoryGirl.create(:user)
+    it "will successfully create a new game in our database as Sith" do
       sign_in user
-
       count = Game.all.length
       post :create, sith_user_id: user.id 
       count2 = Game.all.length
+
       expect(count2).to eq(count + 1)
       expect(response).to redirect_to games_path
-      
     end
 
-    it "should properly deal with validation errors" do
-      user = FactoryGirl.create(:user)
+    it "will properly deal with validation errors" do
       sign_in user
-
       post :create
+
       expect(response).to have_http_status(:unprocessable_entity)
     end  
   end
 
   describe "games#update action"  do
-    it "should allow users to join a game as Jedi" do
-      user = FactoryGirl.create(:user)
-      user2 = FactoryGirl.create(:user)
-      sign_in user
-      g = FactoryGirl.create(:game, sith_user_id: user2.id) 
 
-      patch :update, id: g.id, jedi_user_id: user.id
-      expect(response).to redirect_to ships_path 
+    context "valid join request" do
+      it "will allow authenticated user to join a game as Jedi" do
+        sign_in user
+        g = FactoryGirl.create(:game, sith_user_id: user2.id) 
+        patch :update, id: g.id, jedi_user_id: user.id
+        last = Game.last
+
+        expect(last.jedi_user_id).to eq(user.id)
+        expect(response).to redirect_to ships_path 
+        # test that primary game owner is alerted to game update
+      end
+
+      it "will allow authenticated user to join a game as Sith" do
+        sign_in user
+        g = FactoryGirl.create(:game, jedi_user_id: user2.id)
+        patch :update, id: g.id, sith_user_id: user.id
+        last = Game.last
+
+        expect(last.sith_user_id).to eq(user.id)
+        expect(response).to redirect_to ships_path 
+        # test that primary game owner is alerted to game update
+      end
     end
 
-    it "should allow users to join a game as Sith" do
-      user = FactoryGirl.create(:user)
-      user2 = FactoryGirl.create(:user)
-      sign_in user
-      g = FactoryGirl.create(:game, jedi_user_id: user2.id)
+    context "invalid join request" do
 
-      patch :update, id: g.id, sith_user_id: user.id
-      expect(response).to redirect_to ships_path
-    end
-    
-# <<<<<<< HEAD
-#   describe "games#update action" do
-
-#     context "valid credentials" do
-#       before each
-#       context "valid join request" do
-#         it "will successfully update a game with second player" do
-# # test that the record is updated
-# # test that current_user is redirected to the ship staging page
-# # test that primary game owner is alerted to game update
-#         end
-#       end
-
-#       context "invalid join request" do
 #         it "will prevent user from joining their own game" do
 # # test adding an error in flash
 # # test redirecting to game detail
 #         end
-#       end
-#     end
 
-#     context "invalid credentials" do
 #       it "will require user to be logged in" do
 # # test error/alert
 # # test redirect to new_user_session_path or new_user_registration_path?
 #       end
-#     end
 
-# =======
-# >>>>>>> master
+    end
   end
-
 end
