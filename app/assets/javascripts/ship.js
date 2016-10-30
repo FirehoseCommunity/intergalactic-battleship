78c689af320597e21e3ca08a992cafe0cb06d551
Kamakshi <kpat108@gmail.com>
2016-10-30 01:17:38 +0000

    --skip-ci added rotation with conditions on boundaries

--- app/assets/javascripts/ship.js
+++ app/assets/javascripts/ship.js
@@ -1,68 +1,56 @@
 $(function () {
-
    
   $('.square').droppable({
     tolerance: 'pointer',
     snap: '.square',
-    drop: function(event, ui) {
-      var piece = $(".lastClicked");
-      var ship = piece.data("shipId");
-      var x = $(event.target).data("xcoord");
-      var y = $(event.target).data("ycoord");
-      var length = piece.data("length");
-      var endPt = x + length - 1;
-      var name = piece.data("name");
-      var shipAttributes = {
-        x: x, 
-        y: y, 
-        ship_id: ship,
-        length: length,
-        endpt: endPt,
-        name: name
-      };
-
-      console.log(shipAttributes);
-      var drop_p = $(this).offset();
-      var drag_p = ui.draggable.offset();
-      var left_end = drop_p.left - drag_p.left + 1;
-      var top_end = drop_p.top - drag_p.top + 1;
-      if(endPt > 10) {
-         ui.draggable.draggable({revert: "invalid"});
-         
-      } else {
-        ui.draggable.animate({
-          top: '+=' + top_end,
-          left: '+=' + left_end,
-        });
-      }
-    }
+    drop: handleDrop
   });
 
+  function handleDrop(event, ui) {
+    var piece = $(".lastClicked");
+    var ship = piece.data("shipId");
+    var x = $(event.target).data("xcoord");
+    var y = $(event.target).data("ycoord");
+    var length = piece.data("length");
+    var endPt = x + length - 1;
+    var endY = y + length - 1 ;
+    var name = piece.data("name");
+    var shipAttributes = {
+      x: x, 
+      y: y, 
+      ship_id: ship,
+      length: length,
+      endpt: endPt,
+      endY: endY,
+      name: name
+    };
+    console.log(shipAttributes);
+    var drop_p = $(this).offset();
+    var drag_p = ui.draggable.offset();
+    var left_end = drop_p.left - drag_p.left + 1;
+    var top_end = drop_p.top - drag_p.top + 1;
+    if(endPt > 10) {
+        ui.draggable.draggable({revert: "invalid"});        
+    } else {
+      ui.draggable.animate({
+        top: '+=' + top_end,
+        left: '+=' + left_end,
+      });
+    }
+
+    if((endY > 10) || (x == 1)) {
+      $(piece).css("transform", "rotate(0deg)");
+    }
+  }
+  
   $('.draggable').draggable({
     containment: "#wrapper",
     revert: 'invalid',
     scroll: false,
     stack: ".square",
-    opacity: 0.8, 
+    opacity: 0.6, 
     cursor: "move",
-    handle: ".drag",
-
-  });
-
-  $( ".rotate" ).click(function() {
-    var ship = $(".lastClicked");
-    var coord = ship.data('x');
-    console.log(coord);
-    if(coord == 1) {
-      console.log('cannot rotate')
-        $(this).css("transform","" );
-    }
-
-    if ($(this).css( "transform" ) == 'none') { 
-        $(this).css("transform","rotate(90deg)").addClass("vert");
-    } else {
-        $(this).css("transform","" );
-    }
+    handle: ".drag"
   });
 
   $('.piece').mousedown(function(){
@@ -71,8 +59,14 @@ $(function () {
     $(this).addClass('lastClicked');
   });
 
-
-
+  $('#vert').on("click", function(){
+    var piece = $('.lastClicked');
+ 
+    if($(piece).css("transform") == 'none') {
+      $(piece).css("transform", "rotate(90deg)").addClass('rotate');
+      $(piece).css("transform");
+    };
+  });
 
 
 });
